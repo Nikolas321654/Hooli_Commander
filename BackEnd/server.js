fe0a831e96b98fb07bb39162a32969b28da662a6
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2025-04-04 00:36:47 +0300

    - Removed express and cors modules - Replaced express with native requests processing

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,33 +1,51 @@
+const http = require('http');
+const url = require('url');
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 const { getDisks, getDir } = require('./modules/fs');
 const port = process.env.PORT || 3000;
-const app = express();
-app.use(cors());
 
-app.get('/disks', async (req, res) => {
-  try {
-    const disks = await getDisks();
-    res.send(disks);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message);
-  }
-});
+http
+  .createServer(async function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-app.get('/dir', async (req, res) => {
-  try {
-    const data = await getDir(req.query.path);
-    res.send(data);
-  } catch (error) {
-    console.log(error);
-    if (error.code === 'ENOENT') return res.status(404).send('Not found');
-    if (error.code === 'EACCES') return res.status(403).send('Access denied');
-    return res.status(500).send('Unknown error');
-  }
-});
+    if (req.method === 'GET' && req.url === '/disks') {
+      try {
+        const disks = await getDisks();
+        res.end(JSON.stringify(disks));
+        return;
+      } catch (error) {
+        console.log(error);
+        res.statusCode = 500;
+        res.end(error.message);
+        return;
+      }
+    }
 
-app.listen(port, () => {
-  console.log(`Hooli Commander is listening on port ${port}`);
-});
+    if (req.method === 'GET' && req.url.startsWith('/dir')) {
+      try {
+        const path = url.parse(req.url, true).query.path;
+        const data = await getDir(path);
+        res.end(JSON.stringify(data));
+      } catch (error) {
+        console.log(error);
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Not found');
+        } else if (error.code === 'EACCES') {
+          res.statusCode = 403;
+          res.end('Access denied');
+        } else {
+          res.statusCode = 500;
+          res.end('Unknown error');
+        }
+      }
+    }
+  })
+  .listen(port, () => {
+    console.log(`Hooli Commander is listening on port ${port}`);
+  });
